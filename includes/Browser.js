847aaf93cda980c679474e9237bcccecf74bbae4
Syed MH <hassan@syedmh.com>
2021-07-30 11:35:38 +0500

    Added support for multiple URls

--- includes/Browser.js
+++ includes/Browser.js
@@ -14,6 +14,11 @@ module.exports = {
   _pageEvents: require("./page/PageEvents.js"),
 
   /**
+   * File system module
+   */
+  _fileSystem: require("./FileSystem.js"),
+
+  /**
    * Create a browser instance
    *
    * @param { boolean } HEADLESS
@@ -69,7 +74,18 @@ module.exports = {
    * @return { void }
    */
   run: async function (
-    URL = "https://www.byrdie.com/skin-tint-ranking-5193855",
+    URLS = [
+      "https://www.byrdie.com/skin-tint-ranking-5193855",
+      "https://www.byrdie.com/simone-biles-olympics-setting-boundaries-5194665",
+      "https://www.byrdie.com/celebrity-gray-hair-5194472",
+      "https://www.byrdie.com/dolly-parton-interview-5194488",
+      "https://www.byrdie.com/savannah-lee-smith-gossip-girl-skincare-routine-5193167",
+      "https://www.byrdie.com/comfortable-wedding-shoes-5194016",
+      "https://www.byrdie.com/vitamin-a-deficiency-symptoms-5192633",
+      "https://www.byrdie.com/gemma-chan-interview-5119931",
+      "https://www.byrdie.com/diy-charcoal-face-mask-5080724",
+    ],
+    PROJECT_IDENTIFIER = "scraper-development-trials",
     HEADLESS = false
   ) {
     const browser = await this._initBrowser(HEADLESS);
@@ -80,7 +96,7 @@ module.exports = {
     });
     let initContent;
     do {
-      await page.goto(URL);
+      await page.goto(URLS[0]);
       await page.addScriptTag({
         path: "includes/page/PageEvents.js",
       });
@@ -90,5 +106,21 @@ module.exports = {
       initContent = await page.evaluate(this._initScraper);
     } while (!initContent);
     console.log(initContent);
+    const initContentFiles = {
+      html: this._fileSystem.createFile(
+        "init-content",
+        "html",
+        initContent.map((node) => node.content).join(""),
+        PROJECT_IDENTIFIER,
+        "init"
+      ),
+      json: this._fileSystem.createFile(
+        "init-content",
+        "json",
+        JSON.stringify(initContent, null, 2),
+        PROJECT_IDENTIFIER,
+        "init"
+      ),
+    };
   },
 };
